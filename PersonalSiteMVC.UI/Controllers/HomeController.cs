0c1ee66a84eaa771c4e4b89077c9f3c2047b75ee
Jae Taylor <jjtaylor@mobileluminaries.com>
2021-06-30 12:45:03 -0400

    Converted PSv1 site to MVC structure

--- PersonalSiteMVC.UI/Controllers/HomeController.cs
+++ PersonalSiteMVC.UI/Controllers/HomeController.cs
@@ -1,29 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PersonalSiteMVC.UI.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
+        public ActionResult Links()
+        {
             return View();
         }
+
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+
+        public ActionResult Skills()
+        {
+            return View();
+        }
+
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+
+        public ActionResult projectDetails()
+        {
             return View();
         }
     }
